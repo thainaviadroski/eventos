a56484defac7e949675ad7eded5daf80d2b2e23a
thainã <thainav2010@gmail.com>
2026-03-29 00:26:54 -0300

    CHORE: Spring docs enable
    Adição de dependencia para documentação da api e, implementação dos Beans de configuração da documentação dentro do projeto.

---
+++ src/main/java/net/eventos_facu/eventos/config/WebConfig.java
@@ -0,0 +1,9 @@
+package net.eventos_facu.eventos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+}
