4aea42c3a886bca8960ba3fd6240d4aba0fd2b32
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2026-04-09 07:55:44 -0300

    Otimização nas importações

--- src/main/java/net/eventos_facu/eventos/dto/certificados/CertificadoResponseDto.java
+++ src/main/java/net/eventos_facu/eventos/dto/certificados/CertificadoResponseDto.java
@@ -1,11 +1,8 @@
 package net.eventos_facu.eventos.dto.certificados;
 
-import jakarta.validation.constraints.NotNull;
-import net.eventos_facu.eventos.dto.eventos.EventosResponseDto;
 import net.eventos_facu.eventos.entities.TiposCertificados;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 
 public record CertificadoResponseDto(
         Long id,
