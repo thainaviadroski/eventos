1505d9a7e7da3859d2f9b618f3b4ea291163f158
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2026-04-09 07:56:46 -0300

    Otimização nas importações do Lombok

--- src/main/java/net/eventos_facu/eventos/entities/CertificadoImages.java
+++ src/main/java/net/eventos_facu/eventos/entities/CertificadoImages.java
@@ -1,10 +1,7 @@
 package net.eventos_facu.eventos.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "certificado_images")
 public class CertificadoImages {
 
