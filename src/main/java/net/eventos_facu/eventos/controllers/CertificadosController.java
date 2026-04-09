460518a6d95e15a6366ceb486add00f5d0bc5e50
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2026-04-09 07:55:11 -0300

    FEAT: Implementação do findAll, findBy id e findByEvento

--- src/main/java/net/eventos_facu/eventos/controllers/CertificadosController.java
+++ src/main/java/net/eventos_facu/eventos/controllers/CertificadosController.java
@@ -3,15 +3,21 @@ package net.eventos_facu.eventos.controllers;
 
 import lombok.RequiredArgsConstructor;
 import net.eventos_facu.eventos.dto.certificados.CertificadoRequestDto;
+import net.eventos_facu.eventos.dto.certificados.CertificadoResponseDto;
 import net.eventos_facu.eventos.entities.Certificados;
 import net.eventos_facu.eventos.services.CertificadosService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 
@@ -25,26 +31,27 @@ public class CertificadosController {
     private final CertificadosService service;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void createNewCertificado(@RequestPart("certificado") CertificadoRequestDto certificado,
-                                     @RequestPart("fundo") MultipartFile fundo) {
-        logger.info("Info certificado: {}", certificado.toString());
-        logger.info("Imagem:{}", fundo.getOriginalFilename());
-
-        try {
-            service.createNewCertificado(certificado, fundo);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+    public ResponseEntity<CertificadoResponseDto> createNewCertificado(@RequestPart("certificado") CertificadoRequestDto certificado,
+                                                                       @RequestPart("fundo") MultipartFile fundo) throws IOException {
+        logger.info("Request new certificado: {}", certificado.toString());
+        CertificadoResponseDto result = service.createNewCertificado(certificado, fundo);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(result.id()).toUri();
+        return ResponseEntity.created(location).body(result);
     }
 
     @GetMapping
-    public List<Certificados> getAllCertificados() {
-        return null;
+    public Page<CertificadoResponseDto> getAllCertificados(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable)).getBody();
     }
 
     @GetMapping("/{id}")
-    public void getCertificadoById(@PathVariable Long id) {
+    public ResponseEntity<CertificadoResponseDto> getCertificadoById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping("/evento/{eventoId}")
+    public ResponseEntity<List<CertificadoResponseDto>> getCertificadoByEvento(@PathVariable Long eventoId) {
+        return ResponseEntity.ok(service.findByEventoId(eventoId));
     }
 
     @PutMapping
